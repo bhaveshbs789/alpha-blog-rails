374fb2a2bc4e77c86f07a13a8ed63700ed023102
Bhavesh Sharma - Ruby on Rails <bhaveshbs78901@gmail.com>
2016-12-14 06:07:36 +0000

    Create new action, template and route

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   
   get 'about' => 'pages#about'
   
-  resources :articles
+  resources :articles#this line gives the option to Create, Read, Update , Delete articles
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
